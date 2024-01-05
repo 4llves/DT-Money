75fb0728b9fbb711a85c6f2a52bbb7bd07cc7422
4llves <jhonatab4@gmail.com>
2024-01-04 23:01:27 -0300

    Componente Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,41 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import {  SummaryCard, SummaryContainer } from "./styles";
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>
+            Enradas
+            <ArrowCircleUp size={32} color="#00b37e"/>
+          </span>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>
+            Saídas
+            <ArrowCircleDown size={32} color="#f75a68"/>
+          </span>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>
+            Total
+            <CurrencyDollar size={32} color="#fff"/>
+          </span>
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
