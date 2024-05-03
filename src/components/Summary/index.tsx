680520711223133f94458585a1a45ad9f80b501c
4llves <jhonatab4@gmail.com>
2024-05-03 10:34:29 -0300

    implementing: hook summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,16 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import {  SummaryCard, SummaryContainer } from "./styles";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { useContext } from "react";
-import { priceFormatter } from "../../utils/formatter";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { SummaryCard, SummaryContainer } from './styles'
+import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext)
-
-  const summary = transactions.reduce((acc, transaction) => {
-    if(transaction.type === 'income') {
-      acc.income += transaction.price
-      acc.total += transaction.price
-    } else {
-      acc.outcome += transaction.price
-      acc.total -= transaction.price
-    }
-
-    return acc
-  }, {income: 0, outcome: 0, total: 0})
-
+  const summary = useSummary()
   return (
     <SummaryContainer>
       <SummaryCard>
         <header>
-          <span>
-            Enradas
-            <ArrowCircleUp size={32} color="#00b37e"/>
-          </span>
+          <span>Enradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
         <strong>{priceFormatter.format(summary.income)}</strong>
@@ -34,10 +18,8 @@ export function Summary() {
 
       <SummaryCard>
         <header>
-          <span>
-            Saídas
-            <ArrowCircleDown size={32} color="#f75a68"/>
-          </span>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
         <strong>{priceFormatter.format(summary.outcome)}</strong>
@@ -45,10 +27,8 @@ export function Summary() {
 
       <SummaryCard variant="green">
         <header>
-          <span>
-            Total
-            <CurrencyDollar size={32} color="#fff"/>
-          </span>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
         </header>
 
         <strong>{priceFormatter.format(summary.total)}</strong>
